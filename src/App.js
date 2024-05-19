75cce58ea3cc0b3eaa32042aeee1208c8ad85926
haram8009 <haram8009@gmail.com>
2024-05-19 15:14:30 +0900

    feat: pages 추가, 로그인 회원가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Login, SignUp } from "./pages";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<div>메인페이지</div>}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
