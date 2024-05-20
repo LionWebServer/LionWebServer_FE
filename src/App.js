3eacb48d1e0289a14773278aeed9beee10c70fef
haram8009 <haram8009@gmail.com>
2024-05-20 11:08:37 +0900

    feat: 헤더 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Login, SignUp } from "./pages";
+import { Header } from "./components";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<div>메인페이지</div>}></Route>
         <Route path="/login" element={<Login />} />
