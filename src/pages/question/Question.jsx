cf6f2c512c02e235b7030cb99e27c14d7ea801b3
minjeoong <orange_mj@naver.com>
2024-05-22 12:42:12 +0900

    feat: Question list 임시 생성

--- src/pages/question/Question.jsx
+++ src/pages/question/Question.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { DataGrid } from '@mui/x-data-grid';
+
+
+const rows= [
+  { id: 1, col1: '1', col2: '질문1', col3 : '2024년 5월 22일' },
+  { id: 2, col1: '2', col2: '질문2', col3 : '2024년 5월 24일' },
+  { id: 3, col1: '3', col2: '질문3', col3 : '2024년 5월 25일' },
+];
+
+const columns = [
+  { field: 'col1', headerName: '번호', width: 150 },
+  { field: 'col2', headerName: '제목', width: 150 },
+  { field: 'col3', headerName: '작성일자', width: 150 },
+];
 
 export default function Question() {
-  return <>질문목록페이지</>;
+
+  return (
+    <div style={{ height: 300, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+  );
 }
+
