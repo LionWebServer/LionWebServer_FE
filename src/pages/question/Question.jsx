75d989e7ce383cfad9934a60b983111037b0ac11
minjeoong <orange_mj@naver.com>
2024-05-22 20:07:11 +0900

    feat: Grid 리펙터링

--- src/pages/question/Question.jsx
+++ src/pages/question/Question.jsx
@@ -2,19 +2,7 @@ import React, {useEffect, useState} from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import {useNavigate} from "react-router-dom";
 import {useGetQuestion} from "../../hooks/useGetQuestion";
-
-
-const rows= [
-  { id: 1, col1: '1', col2: '질문1', col3 : '2024년 5월 22일' },
-  { id: 2, col1: '2', col2: '질문2', col3 : '2024년 5월 24일' },
-  { id: 3, col1: '3', col2: '질문3', col3 : '2024년 5월 25일' },
-];
-
-const columns = [
-  { field: 'col1', headerName: '번호', width: 100},
-  { field: 'col2', headerName: '제목', width: 700 },
-  { field: 'col3', headerName: '작성일자', type: 'dateTime', width: 250, valueGetter: (value) => value && new Date(value)},
-];
+import {rows, columns} from "../../config/QuestionGrid";
 
 export default function Question() {
   const {GetQuestion, data} = useGetQuestion();
