3d4b5c3972b39858291cbfad0eee3d315fcae09e
haram8009 <haram8009@gmail.com>
2024-05-29 00:36:41 +0900

    feat: 서버와 auth api 연결

--- src/api/accountAPI.js
+++ src/api/accountAPI.js
@@ -3,21 +3,21 @@ import Api from "./api";
 
 const auth = {
   signUp(data) {
-    return axios.post("/server/api/auth/sign-up/", data);
+    return axios.post("/server/api/auth/sign-up", data);
   },
   login(data) {
     return axios.post("/server/api/auth/sign-in", data);
   },
   logout() {
     // FIXME
-    return Api.post("/server/api/logout/");
+    // return Api.post("/server/api/logout");
   },
   refresh(data) {
     // FIXME
-    return axios.post(`/server/api/refresh/`, data);
+    // return axios.post(`/server/api/refresh`, data);
   },
   question() {
-    return axios.get("/server/api/question-list/" );
+    return axios.get("/server/api/question-list");
   },
 };
 
